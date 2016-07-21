d2f6b61b8aaeebad7db1286cbe2b8ec9d6e249f1
barrettzhang <735398606@qq.com>
2016-07-21 18:02:59 +0800

    201607211800

--- dist/js/children.js
+++ dist/js/children.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	$a = $(".navbar a");
 	$a.eq(0).find("span").css("color","#febe2f");
 	$a.eq(0).css("marginTop","-70px");
+	$ul = $(".wrap ul");
+	$ul.eq(0).css('display', 'block');
 	$a.on('click',animated);
 	function animated(){
 		$this = $(this);
@@ -9,5 +11,8 @@ $(document).ready(function(){
 		$this.find("span").css("color","#febe2f");
 		$this.siblings("a").animate({marginTop:'0'});
 		$this.siblings("a").find("span").css("color","#434343");
+		$t = $this.index();
+		$ul.css('display','none');
+		$ul.eq($t).css('display', 'block');
 	};	
 });
