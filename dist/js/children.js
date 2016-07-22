c002986fb014bcf96147b66abbcb6dd8d6dea2af
barrettzhang <735398606@qq.com>
2016-07-22 09:42:45 +0800

    201607220942

--- dist/js/children.js
+++ dist/js/children.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$a = $(".navbar a");
-	$a.eq(0).find("span").css("color","#febe2f");
+	$a.eq(0).find("span").addClass('spancolor');
 	$a.eq(0).css("marginTop","-70px");
 	$ul = $(".wrap ul");
 	$ul.eq(0).css('display', 'block');
@@ -9,9 +9,9 @@ $(document).ready(function(){
 	function animated(){
 		$this = $(this);
 		$this.animate({marginTop:'-70px'});
-		$this.find("span").css("color","#febe2f");
+		$this.find("span").addClass('spancolor');
 		$this.siblings("a").animate({marginTop:'0'});
-		$this.siblings("a").find("span").css("color","#434343");
+		$this.siblings("a").find("span").removeClass('spancolor');
 		$t = $this.index();
 		$ul.css('display','none');
 		$ul.eq($t).css('display', 'block');
