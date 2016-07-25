cf74b130714c5cadf18113deeb6c510cd3c28a1d
barrettzhang <735398606@qq.com>
2016-07-25 15:27:14 +0800

    201607251527

--- dist/js/beach.js
+++ dist/js/beach.js
@@ -1,17 +1,12 @@
 $(document).ready(function(){
-	$li = $(".navbar li");
-	$ul = $(".liebiao ul");
-	$ul.eq(0).css('display','block');
-	$li.find('a').addClass('abackground2');
-	$li.find('a').eq(0).addClass('abackground1');
-	$li.on('click',function(){
-		$this = $(this);
-		$t = $this.index();
-		$this.find('a').addClass('abackground1');
-		$this.siblings('li').find('a').removeClass('abackground1');
-		$ul.css("display","none");
-		$ul.eq($t).css("display","block");
-	})
-	
+	$(function(){
+        var _index=0;
+        $("#navigationbar ul li").click(function(){
+            $(this).addClass("beachStyle").siblings("li").removeClass("beachStyle");
+            _index=$(this).index()+1;
+            var _top=$("#louti"+_index).offset().top;
+            $("body,html").animate({scrollTop:_top},500);
+        }); 
+    });
 		
 });
--- dist/js/children.js
+++ dist/js/children.js
@@ -1,19 +1,11 @@
 $(document).ready(function(){
-	$a = $(".navbar a");
-	$a.eq(0).find("span").addClass('spancolor');
-	$a.eq(0).css("marginTop","-70px");
-	$ul = $(".wrap ul");
-	$ul.eq(0).css('display', 'block');
-	$a.on('click',animated);
-	
-	function animated(){
-		$this = $(this);
-		$this.animate({marginTop:'-70px'});
-		$this.find("span").addClass('spancolor');
-		$this.siblings("a").animate({marginTop:'0'});
-		$this.siblings("a").find("span").removeClass('spancolor');
-		$t = $this.index();
-		$ul.css('display','none');
-		$ul.eq($t).css('display', 'block');
-	};	
+	$(function(){
+        var _index=0;
+        $(".navbar a").click(function(){
+            $(this).addClass("navbarStyle").siblings("a").removeClass("navbarStyle");
+            _index=$(this).index()+1;
+            var _top=$("#louti"+_index).offset().top;
+            $("body,html").animate({scrollTop:_top},500);
+        });
+    });
 });
