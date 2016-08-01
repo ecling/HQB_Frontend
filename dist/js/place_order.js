73898c6da0981b0c101602ecbecb0211e983625d
ling php <php@highqb.com>
2016-08-01 18:20:05 +0800

    201608011820

--- dist/js/place_order.js
+++ dist/js/place_order.js
@@ -4,16 +4,22 @@
 
 $(function(){
 	(function(){
-		var fixContainer = $('.rightD');
+		var fixContainer = $('.fixbox');
 		var offset = fixContainer.offset();
 		$(window).scroll(function(){
 			var top = $(document).scrollTop();
 			if(top-offset.top>0){
 				fix_top = top-offset.top;
-				
-				$('.fixbox').css({'top':fix_top},100);
+				$('.fixbox').css({'top':fix_top});
 			}else{
 
+			}
+		});
+		$("#commentForm").delegate('.payments button','click',function(){
+			var top = $(document).scrollTop();
+			if(top-offset.top>0){
+				fix_top = top-offset.top;
+				$('.fixbox').css({'top':fix_top});
 			}
 		});
 	})();
