50c52b10a1da694972a230cb41eb16b70b391fc9
barrett <735398606@qq.com>
2016-08-01 18:35:07 +0800

    20160801183

--- dist/js/place_order.js
+++ dist/js/place_order.js
@@ -1,5 +1,5 @@
 (function($){
-
+		
 })(jQuery);
 
 $(function(){
@@ -23,4 +23,21 @@ $(function(){
 			}
 		});
 	})();
+
+	$(".clicked").on('click',show);
+	$(".contentd a").on('click',show);
+	$(".icon-close").on('click',hide);
+	$("#submit").on('click',hide);
+    
+
+    function show(){
+    	$(".popup").show();
+		$(".mask").css("height",$(document).height());     
+        $(".mask").css("width",$(document).width());     
+        $(".mask").show(); 
+    }
+    function hide(){
+    	$(".popup").hide();
+		$(".mask").hide();
+    }
 });
