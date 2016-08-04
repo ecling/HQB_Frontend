c9e8bf70016cb0cc46767762e6d52e49804e4ee6
barrett <735398606@qq.com>
2016-08-04 18:53:13 +0800

    201608041853

--- dist/js/myorder.js
+++ dist/js/myorder.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function($) {
 	var $div = $(".Account_Setting_conter>div");
 	$li.on('click', function() {
 		var $this = $(this);
+		$li.removeClass('tab_style');
+		$this.addClass('tab_style');
 		$index = $this.index();
 		$div.css('display','none');
 		$div.eq($index).css('display','block');
