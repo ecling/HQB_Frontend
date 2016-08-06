138fb1a83f5e392deb36b196c0ee43d420edaf7b
barrett <735398606@qq.com>
2016-08-06 17:29:34 +0800

    201608061729

--- dist/js/place_order.js
+++ dist/js/place_order.js
@@ -26,18 +26,30 @@ $(function(){
 
 	$(".clicked").on('click',show);
 	$(".contentd a").on('click',show);
+	$(".icon-xiugai").on('click',show);
+	$(".new_address_a").on('click',show);
 	$(".icon-close").on('click',hide);
 	$("#submit").on('click',hide);
     
 
     function show(){
-    	$(".popup").show();
+    	$(".popup").show(500);
 		$(".mask").css("height",$(document).height());     
         $(".mask").css("width",$(document).width());     
-        $(".mask").show(); 
+        $(".mask").show(500); 
     }
     function hide(){
-    	$(".popup").hide();
-		$(".mask").hide();
+    	$(".popup").hide(500);
+		$(".mask").hide(500);
     }
+
+    $(".conter_address_left .default").on('click',function(){
+    	$this = $(this);
+    	$this.addClass("defaultStyle").html("Make Default");
+    	$this.parent(".conter_address_left").siblings().find(".default").html("Default").removeClass('defaultStyle'); 	
+    });
+    $(".conter_address_left .icon-shanchu").on('click',function(){
+    	$this = $(this);
+    	$this.parent(".conter_address_left").animate({"marginTop":"-2000px",}, 1000);
+    });
 });
