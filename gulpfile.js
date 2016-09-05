79e56d770a7ff3ef363f14e4a53e76bed21b970c
ling php <php@highqb.com>
2016-09-05 17:42:49 +0800

    201609051742

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
 var sass = require("gulp-ruby-sass");
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('serve',['sass'],function(){
+gulp.task('serve',['minify-css'],function(){
 	browserSync.init({
         server: "./"
     });
-	gulp.watch("./src/scss/**/*.scss", ['sass']);
+	gulp.watch("./src/scss/**/*.scss", ['minify-css']);
 	gulp.watch("./*.html").on('change', reload);
     gulp.watch("./dist/js/*.js").on('change',reload);
 });
@@ -22,9 +24,22 @@ gulp.task('sass',function(){
             includeContent: false,
             sourceRoot: '../../src/scss/pages/'
         }))
+        .pipe(gulp.dest('./src/css/'));
+});
+
+gulp.task('autoprefixer',['sass'],function(){
+    return gulp.src('./src/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./src/css/'));
+});
+
+gulp.task('minify-css',['autoprefixer'], function(){
+    return gulp.src('./src/css/*.css')
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./dist/css/'))
         .pipe(browserSync.stream());
 });
-
 
 gulp.task('default', ['serve']);
